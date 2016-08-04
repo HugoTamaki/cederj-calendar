44e6a12ed44d4aee98a7e9025dc2c415149783ae
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-08-04 09:50:11 -0300

    Initialising service with course value

--- src/js/services/courses_service.js
+++ src/js/services/courses_service.js
@@ -1,20 +1,28 @@
 app.service('CoursesService', [
-  '$scope',
-  '$state',
-  '$http',
-  '$q',
+  'DataService',
 
-  function ($scope,
-            $state,
-            $http,
-            $q) {
+  function (DataService) {
 
     var CoursesService = {
-      getCourses: function () {
-        $http.get('./courses.json')
-          .success(function (data) {
-            console.log(data)
-          })
+      courses: [],
+      init: function () {
+        var courses = DataService.courses,
+            disciplines = DataService.disciplines,
+            self = this
+
+        this.courses = courses.map(function (course) {
+          return self.getDisciplines(course)
+        })
+      },
+
+      getDisciplines: function (course) {
+        var disciplines = DataService.disciplines
+
+        course.disciplines = disciplines.filter(function (discipline) {
+          return discipline.course === course.name
+        })
+
+        return course
       }
     }
 
