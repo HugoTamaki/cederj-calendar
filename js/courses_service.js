3a1d884c949f4420cf2efa1ca1c90d3c1572b8c8
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-08-21 17:47:00 -0300

    Initial www subtree commit

---
+++ js/courses_service.js
@@ -0,0 +1,57 @@
+app.service('CoursesService', [
+  'DataService',
+
+  function (DataService) {
+
+    var CoursesService = {
+      courses: [],
+      selectedCourse: null,
+      term: '',
+      init: function () {
+        var locations = DataService.locations,
+            self = this
+
+        this.locations = locations.map(function (location) {
+          return self.getCourses(location)
+        })
+        
+        this.locations.forEach(function (location) {
+          location.courses.map(function (course) {
+            return self.getDisciplines(course)
+          })
+        })
+      },
+
+      getCourses: function (location) {
+        var courses = DataService.courses
+
+        location.courses = courses.filter(function (course) {
+          return course.location === location.name
+        })
+
+        return location
+      },
+
+      getDisciplines: function (course) {
+        var disciplines = DataService.disciplines
+
+        course.disciplines = disciplines.filter(function (discipline) {
+          return discipline.course === course.name && discipline.location === course.location
+        })
+
+        return course
+      },
+
+      getFilteredDisciplines: function (course) {
+        var search = this.term.split(" ").join("|")
+        var regexp = new RegExp(search, "i")
+
+        return _(course.disciplines).filter(function (discipline) {
+          return regexp.test(discipline.name)
+        })
+      }
+    }
+
+    return CoursesService
+  }
+])
